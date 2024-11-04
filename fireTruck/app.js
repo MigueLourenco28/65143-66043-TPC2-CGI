28e0a339acdd061706c727bb602bb7f0e97b9265
Miguel <mse.lourenco@campus.fct.unl.pt>
2024-11-04 20:00:38 +0000

    Haed Lights.

--- fireTruck/app.js
+++ fireTruck/app.js
@@ -465,14 +465,38 @@ function setup(shaders) {
             let blueLight = vec4(0.0, 0.0, 1.0, 1);
             let redLight = vec4(1.0, 0.0, 0.0, 1.0);
             let whiteSeperator = vec4(1.0, 1.0, 1.0, 1.0);
-                gl.uniform4fv(u_color, color);
-            multTranslation([-3.7, 3.0, 0.0]);
-            multScale([2.25, 3.00, 4.0]);
-            uploadModelView();
-            CUBE.draw(gl, program, mode);
+            multTranslation([-3.75, 4.6, 0.0]);
             pushMatrix();
-                gl.uniform4fv(u_color, outlineColor);
-                CUBE.draw(gl, program, gl.LINES); // Draw cube outline in wireframe
+                gl.uniform4fv(u_color, blueLight);
+                multTranslation([0.0, 0.0, -0.5]);
+                multScale([0.5, 0.25, 0.5]);
+                uploadModelView();
+                CUBE.draw(gl, program, mode);
+                pushMatrix();
+                    gl.uniform4fv(u_color, outlineColor);
+                    CUBE.draw(gl, program, gl.LINES); // Draw cube outline in wireframe
+                popMatrix();
+            popMatrix();
+            pushMatrix();
+                gl.uniform4fv(u_color, whiteSeperator);
+                multScale([0.5, 0.25, 0.5]);
+                uploadModelView();
+                CUBE.draw(gl, program, mode);
+                pushMatrix();
+                    gl.uniform4fv(u_color, outlineColor);
+                    CUBE.draw(gl, program, gl.LINES); // Draw cube outline in wireframe
+                popMatrix();
+            popMatrix();
+            pushMatrix();
+                gl.uniform4fv(u_color, redLight);
+                multTranslation([0.0, 0.0, 0.5]);
+                multScale([0.5, 0.25, 0.5]);
+                uploadModelView();
+                CUBE.draw(gl, program, mode);
+                pushMatrix();
+                    gl.uniform4fv(u_color, outlineColor);
+                    CUBE.draw(gl, program, gl.LINES); // Draw cube outline in wireframe
+                popMatrix();
             popMatrix();
         popMatrix();
         //------Head Lights------//
@@ -621,7 +645,7 @@ function setup(shaders) {
     }
 
 
-    // Decals
+    // Decals - Write each character on the sides of the truck
     function decalC() {
         pushMatrix();
 
